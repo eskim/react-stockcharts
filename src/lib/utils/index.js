e8b6b3fb013dca6bec168e7a1733ce854d98c00e
Eunsub Kim <eunsub@gmail.com>
2021-05-22 09:07:32 +0900

    fix

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,7 +1,8 @@
 
 
 
-import { scaleOrdinal, schemeCategory10 } from  "d3-scale";
+import { scaleOrdinal } from  "d3-scale";
+const schemeCategory10 = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
 import { bisector } from "d3-array";
 import noop from "./noop";
 import identity from "./identity";
